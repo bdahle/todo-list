fd3af593db684feb309b15e6e2505a2aec97ce3e
Bjørn Harbo Dahle <bdahle@hey.com>
2021-07-07 16:18:04 +0200

    uninstall webpack html plugin

--- main.js
+++ main.js
@@ -9,13 +9,13 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/index.js":
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
+/***/ "./src/js/index.js":
+/*!*************************!*\
+  !*** ./src/js/index.js ***!
+  \*************************/
 /***/ (() => {
 
-eval("\n\n//# sourceURL=webpack://todo-list/./src/index.js?");
+eval("console.log(\"test\");\n\n\n//# sourceURL=webpack://todo-list/./src/js/index.js?");
 
 /***/ })
 
@@ -26,7 +26,7 @@ eval("\n\n//# sourceURL=webpack://todo-list/./src/index.js?");
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
 /******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	__webpack_modules__["./src/js/index.js"]();
 /******/ 	
 /******/ })()
 ;
