d31691f9b090831dd57558b8482be4ee21eb6eae
Bjørn Harbo Dahle <bdahle@hey.com>
2021-07-07 16:18:04 +0200

    uninstall webpack html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,10 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: "./src/js/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
-  plugins: [new HtmlWebpackPlugin()],
 };
