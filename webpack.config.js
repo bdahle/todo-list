d47e9056571ef813f7e13dfefeb0c830944c0035
Bjørn Harbo Dahle <bdahle@hey.com>
2021-07-08 08:42:31 +0200

    rename js files

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: "./src/js/index.js",
+  entry: "./src/js/controller.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
