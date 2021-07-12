0fa2c51b470dc8c90fcb65b8e8494eddd39a6eb4
Bjørn Harbo Dahle <bdahle@hey.com>
2021-07-12 12:52:42 +0200

    separate js into mvc files

--- main.js
+++ main.js
@@ -7,26 +7,8 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
-
-/***/ "./src/js/mvc.js":
-/*!***********************!*\
-  !*** ./src/js/mvc.js ***!
-  \***********************/
-/***/ (() => {
-
-eval("// model /////////////\ntodoList = [];\n\nfunction Todo(name, isDone) {\n  this.name = name;\n  this.isDone = isDone;\n}\n\nfunction modelAddTodo(name) {\n  todoList.push(new Todo(name, false));\n  viewRenderList();\n}\n\nfunction modelRemoveTodo(index) {\n  todoList.splice(index, 1);\n  viewRenderList();\n}\n/////////////////////\n//\n//\n//\n// view /////////////\nconst todoListElement = document.getElementById(\"todoList\");\n\nfunction clearInputField() {\n  newTodoInput.value = \"\";\n}\n\nfunction createTodoElement(newTodo, index) {\n  const newTodoElement = document.createElement(\"div\");\n  const toggleButton = document.createElement(\"button\");\n  toggleButton.innerHTML = \"Toggle\";\n  toggleButton.addEventListener(\"click\", () => {\n    newTodo.isDone = !newTodo.isDone;\n    viewRenderList();\n  });\n  const todoName = document.createElement(\"p\");\n  todoName.innerHTML = newTodo.name + \" \" + newTodo.isDone;\n  const removeButton = document.createElement(\"button\");\n  removeButton.innerHTML = \"DEL\";\n  removeButton.addEventListener(\"click\", () => {\n    modelRemoveTodo(index);\n  });\n  newTodoElement.appendChild(toggleButton);\n  newTodoElement.appendChild(todoName);\n  newTodoElement.appendChild(removeButton);\n\n  return newTodoElement;\n}\n\nfunction viewClearList() {\n  todoListElement.innerHTML = \"\";\n}\n\nfunction viewRenderList() {\n  viewClearList();\n  todoList.forEach((todo, index) => {\n    viewAddTodo(todo, index);\n  });\n}\n\nfunction viewAddTodo(newTodo, index) {\n  clearInputField();\n\n  const newTodoElement = createTodoElement(newTodo, index);\n\n  todoListElement.appendChild(newTodoElement);\n}\n/////////////////////\n//\n//\n//\n// controller /////////////\nconst newTodoInput = document.getElementById(\"newTodoInput\");\nconst newTodoButton = document.getElementById(\"newTodoButton\");\n\nnewTodoButton.addEventListener(\"click\", controllerAddTodo);\n\nfunction controllerAddTodo() {\n  modelAddTodo(newTodoInput.value);\n}\n\nmodelAddTodo(\"Vann plantene\");\nmodelAddTodo(\"Ta ut søppelet\");\nmodelAddTodo(\"Luft hunden\");\n\n/////////////////////\n\n\n//# sourceURL=webpack://todo-list/./src/js/mvc.js?");
-
-/***/ })
-
-/******/ 	});
-/************************************************************************/
+/******/ 	"use strict";
 /******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/js/mvc.js"]();
 /******/ 	
 /******/ })()
 ;
