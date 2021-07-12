57a93d580904f2da95f191afcd021fd443cc12b4
Bjørn Harbo Dahle <bdahle@hey.com>
2021-07-12 09:44:25 +0200

    build out functions

--- src/js/mvc.js
+++ src/js/mvc.js
@@ -1,27 +1,68 @@
-//model
+// model /////////////
 todoList = [];
 
+function Todo(name, isDone) {
+  this.index = todoList.length;
+  this.name = name;
+  this.isDone = isDone;
+}
+
 function modelAddTodo(name) {
-  todoList.push(name);
-  viewRender(name);
+  const newTodo = new Todo(name, false);
+  todoList.push(newTodo);
+  console.log(newTodo.name);
+  const index = todoList.length - 1;
+  viewAddTodo(newTodo);
 }
 
-//view
-function viewRender(name) {
+function modelRemoveTodo(index) {}
+/////////////////////
+//
+//
+//
+// view /////////////
+const todoListElement = document.getElementById("todoList");
+
+function clearInputField() {
   newTodoInput.value = "";
-
-  const newTodo = document.createElement("div");
-  newTodo.innerHTML = name;
-
-  document.body.appendChild(newTodo);
-
-  console.table(todoList);
 }
 
-//controller
+function createTodoElement(newTodo) {
+  const newTodoElement = document.createElement("div");
+  newTodoElement.innerHTML = newTodo.name;
+  newTodoElement.setAttribute("data-index", newTodo.index);
+  return newTodoElement;
+}
+
+function viewAddTodo(newTodo) {
+  clearInputField();
+
+  const newTodoElement = createTodoElement(newTodo);
+
+  newTodoElement.addEventListener("click", () => {
+    console.log(newTodo.index);
+    modelRemoveTodo(newTodo.index);
+  });
+
+  todoListElement.appendChild(newTodoElement);
+}
+
+function viewRemoveTodo() {}
+/////////////////////
+//
+//
+//
+// controller /////////////
 const newTodoInput = document.getElementById("newTodoInput");
 const newTodoButton = document.getElementById("newTodoButton");
 
-newTodoButton.addEventListener("click", () => {
+newTodoButton.addEventListener("click", controllerAddTodo);
+
+function controllerAddTodo() {
   modelAddTodo(newTodoInput.value);
-});
+}
+
+modelAddTodo("Vann plantene");
+modelAddTodo("Ta ut søppelet");
+
+/////////////////////
